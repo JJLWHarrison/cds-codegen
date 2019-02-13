1f2464e332627d57da843a9738df5211c8957d2e
Stuart Low <stuart.low@me.com>
2019-02-13 18:49:46 +1100

    Initial template runner

--- src/main/java/au/csiro/data61/cds/cge/TemplateTest.java
+++ src/main/java/au/csiro/data61/cds/cge/TemplateTest.java
@@ -1,9 +1,11 @@
 package au.csiro.data61.cds.cge;
 
 import java.io.StringWriter;
+import java.util.Properties;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 import org.apache.velocity.app.VelocityEngine;
 import au.csiro.data61.cds.models.api.CommonApi;
 
@@ -15,17 +17,18 @@ public class TemplateTest
 {
     public static void main( String[] args )
     {
-    	VelocityEngine velocityEngine = new VelocityEngine();
-    	velocityEngine.init();
-    	    
-    	Template t = velocityEngine.getTemplate("index.vm");
-    	     
-    	VelocityContext context = new VelocityContext();
+    	
+    	Properties p = new Properties();
+    	p.setProperty("resource.loader", "class");
+    	p.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+    	Velocity.init( p );
+    	VelocityContext context = new VelocityContext();           
+    	Template template = Velocity.getTemplate("test.vm");
     	
     	context.put("api", CommonApi.class);
     	
     	StringWriter writer = new StringWriter();
-    	t.merge( context, writer );
+    	template.merge( context, writer );
     	
     	System.out.println(writer.toString());
     	
