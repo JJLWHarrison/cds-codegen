4e3643feaa0c6797bef6b2e0446c557c14e8cf01
Stuart Low <stuart.low@data61.csiro.au>
2019-02-26 12:42:14 +1100

    Begin variable definition discovery development

--- src/main/java/au/org/consumerdatastandards/cge/ModelBuilder.java
+++ src/main/java/au/org/consumerdatastandards/cge/ModelBuilder.java
@@ -18,6 +18,8 @@ import org.apache.logging.log4j.Logger;
 
 import au.org.consumerdatastandards.interfaces.api.ModelInterface;
 import au.org.consumerdatastandards.models.types.support.Endpoint;
+import au.org.consumerdatastandards.models.types.support.EndpointResponse;
+import au.org.consumerdatastandards.models.types.support.ModelDefinition;
 import au.org.consumerdatastandards.models.types.support.Section;
 
 /**
@@ -44,14 +46,59 @@ public class ModelBuilder {
 		 * Process sections
 		 */
 		modelMap.put("sections", getSectionForest(model.getSections()));
-		
+		modelMap.put("models", getModelForest((ArrayList<Map<String,Object>>) modelMap.get("sections")));
 		
 		return modelMap;
 	}
 	
+	private List<Object> getModelForest(ArrayList<Map<String, Object>> sectionList) {
+		Map<String,ModelDefinition> modelMap = new HashMap<String,ModelDefinition>();
+		
+		for(int i = 0; i < sectionList.size(); i++) {
+			Map<String,Object> oneSection = sectionList.get(i);
+			
+			ArrayList<Endpoint> myEndpoints = (ArrayList<Endpoint>) oneSection.get("endpoints");
+			
+			myEndpoints.forEach((oneEndpoint) -> {
+				EndpointResponse[] responseList = oneEndpoint.responseList();
+				for(int j = 0; j < responseList.length; j++) {
+					modelMap.putAll(getModelMap(responseList[j].content()));
+				}
+			});
+		}
+		
+		return List.copyOf(modelMap.values());
+		
+	}
 	
-	private ArrayList<Object> getSectionForest(Class[] sections) {
-		ArrayList<Object> sectionList = new ArrayList<Object>();
+	private Map<String, ModelDefinition> getModelMap(Class content) {
+		Map<String,ModelDefinition> modelMap = new HashMap<String,ModelDefinition>();
+		
+		System.out.println("Parsing for " + content);
+		Method[] methodList = content.getDeclaredMethods();
+		for(int k = 0; k < methodList.length; k++) {
+			String modelName = methodList[k].getReturnType().getCanonicalName();
+			Map<String,Object> attributeMap = new HashMap<String,Object>();
+			
+			if(modelMap.containsKey(modelName)) {
+				System.out.println(String.format("Duplicate model definition found for %s", modelName));
+				continue;
+			} else if(methodList[k].getReturnType().isPrimitive() || methodList[k].getReturnType().equals(String.class) || methodList[k].getReturnType().isEnum() ) {
+				System.out.println(String.format("Writing primitive variable type for: %s", modelName));
+				attributeMap.put(methodList[k].getName(), methodList[k].getReturnType().getSimpleName());
+				continue;
+			} else {
+				
+				System.out.println("The response list says: " + methodList[k].getReturnType());
+				//modelMap.put(modelName, getModelMap(methodList[k].getReturnType()));
+			}
+		}
+		
+		return modelMap;		
+	}
+
+	private ArrayList<Map<String,Object>> getSectionForest(Class[] sections) {
+		ArrayList<Map<String,Object>> sectionList = new ArrayList<Map<String,Object>>();
 		
 		for(int i = 0; i < sections.length; i++) {
 			sectionList.add(getSection(sections[i]));
@@ -64,7 +111,7 @@ public class ModelBuilder {
 	private Map<String,Object> getSection(Class inputSection) {
 		
 		Map<String,Object> oneSection = new HashMap<String,Object>();
-		ArrayList<Object> myEndpoints = new ArrayList<Object>();
+		ArrayList<Endpoint> myEndpoints = new ArrayList<Endpoint>();
 		
 		/**
 		 * Pull the section class name
@@ -82,11 +129,10 @@ public class ModelBuilder {
 		Method[] fields = inputSection.getDeclaredMethods();
 		for (int j = 0; j < Array.getLength(fields); j++) {
 			System.out.println("Method: " + fields[j].toString());
-			Object myEndpoint = getEndpoint(fields[j]);
+			Endpoint myEndpoint = getEndpoint(fields[j]);
 			if(myEndpoint != null) {
 				myEndpoints.add(myEndpoint);
 			}
-			
 		}
 		
 		oneSection.put("endpoints", myEndpoints);
@@ -95,19 +141,19 @@ public class ModelBuilder {
 		
 	}
 	
-	
-		
-		
-	private Object getEndpoint(Method method) {
+	private Endpoint getEndpoint(Method method) {
 		Annotation[] annotations = method.getAnnotations();
 		for(int k = 0; k < annotations.length; k++) {
-			if(annotations[k].annotationType().equals(au.org.consumerdatastandards.models.types.support.Endpoint.class)) {
-				au.org.consumerdatastandards.models.types.support.Endpoint myAnnotation = (Endpoint) annotations[k];
+			if(annotations[k].annotationType().equals(Endpoint.class)) {
+				Endpoint myAnnotation = (Endpoint) annotations[k];
 				return myAnnotation;
 			}
 		}
-		
-		logger.error("Endpoint for " + method.getName() + " not found");
+
+        logger.error("Endpoint annotation for " + method.getName() + " not found");
 		return null;
 	}
+	
 }
+
+
