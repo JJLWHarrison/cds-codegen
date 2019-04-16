92558fb45633f54c1b9da568cf649d2c72c9073a
fyang1024 <fyang1024@gmail.com>
2019-04-16 16:41:38 +1000

    Refactoring and bug fixes

--- support/samples/cds-holder-java/src/main/java/au/org/consumerdatastandards/holder/api/RestApplication.java
+++ support/samples/cds-holder-java/src/main/java/au/org/consumerdatastandards/holder/api/RestApplication.java
@@ -10,20 +10,13 @@ import java.util.Set;
 @ApplicationPath("/")
 public class RestApplication extends Application {
 
-    private Set<Object> singletons;
     private Set<Class<?>> classes;
 
     public RestApplication() {
-        singletons = new HashSet<>();
         classes = new HashSet<>();
         classes.add(ProductApiServiceImpl.class);
         classes.add(io.swagger.jaxrs.listing.ApiListingResource.class);
         classes.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
-    }
-
-    @Override
-    public Set<Object> getSingletons() {
-        return singletons;
     }
 
     @Override
