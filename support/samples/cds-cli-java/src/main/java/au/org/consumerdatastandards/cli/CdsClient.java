d1e6194a707c68cc01e8b4fa716db5358357d212
Stuart Low <Stuart.Low@data61.csiro.au>
2019-04-21 16:18:22 +1000

    Commit separated cds-cli-java with spring boot shell

---
+++ support/samples/cds-cli-java/src/main/java/au/org/consumerdatastandards/cli/CdsClient.java
@@ -0,0 +1,93 @@
+package au.org.consumerdatastandards.cli;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.UUID;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+import org.threeten.bp.OffsetDateTime;
+
+import au.org.consumerdatastandards.client.ApiClient;
+import au.org.consumerdatastandards.client.ApiException;
+import au.org.consumerdatastandards.client.api.BankingProductsApi;
+import au.org.consumerdatastandards.client.model.ResponseBankingProductById;
+import au.org.consumerdatastandards.client.model.ResponseBankingProductList;
+
+@ShellComponent
+public class CdsClient {
+
+    private final BankingProductsApi api = new BankingProductsApi();
+
+    private String serverUrl;
+
+    @ShellMethod("Set CDS server URL, e.g. http://data.holder/cds-au/v1")
+    public void server(@ShellOption String url) {
+        if (isValid(url)) {
+            this.serverUrl = url;
+            ApiClient client = new ApiClient();
+            client.setBasePath(serverUrl);
+            client.addDefaultHeader("x-v", "1");
+            client.addDefaultHeader("x-min-v", "1");
+            client.addDefaultHeader("x-fapi-financial-id", "cds");
+            client.addDefaultHeader("x-fapi-customer-last-logged-time", "");
+            client.addDefaultHeader("x-fapi-customer-ip-address", "192.168.1.2");
+            client.addDefaultHeader("x-fapi-interaction-id", UUID.randomUUID().toString());
+            api.setApiClient(client);
+            System.out.println("Server URL is set to " + this.serverUrl);
+        } else {
+            System.out.println("Invalid URL, please double check it");
+        }
+    }
+
+    @ShellMethod("List products filtered by the parameters")
+    public void listProducts(
+        @ShellOption(defaultValue = ShellOption.NULL) String effective,
+        @ShellOption(defaultValue = ShellOption.NULL, help = "'yyyy-MM-dd HH:mm:ss.SSS Z' or '-1y -2M -3d 5H 3m 45s 8S'") OffsetDateTime updatedSince,
+        @ShellOption(defaultValue = ShellOption.NULL) String brand,
+        @ShellOption(defaultValue = ShellOption.NULL) String productCategory,
+        @ShellOption(defaultValue = ShellOption.NULL) Integer page,
+        @ShellOption(defaultValue = ShellOption.NULL) Integer pageSize) throws ApiException {
+
+        if (this.serverUrl == null) {
+            System.out.println("Please use `server` command to set Server URL first");
+            return;
+        }
+        try {
+            ResponseBankingProductList response = api.listProducts(effective, updatedSince, brand, productCategory, page, pageSize);
+            System.out.println(response);
+        } catch (Exception e) {
+            System.out.println("Oops, something went wrong!");
+            throw e;
+        }
+    }
+
+    @ShellMethod("Get product detail by id")
+    public void getProductDetail(@ShellOption String id) throws Exception {
+        if (this.serverUrl == null) {
+            System.out.println("Please use `server` command to set Server URL first");
+            return;
+        }
+        try {
+            ResponseBankingProductById productDetail = api.getProductDetail(id);
+            System.out.println(productDetail);
+        } catch (Exception e) {
+            System.out.println("Oops, something went wrong!");
+            throw e;
+        }
+    }
+
+    private boolean isValid(String url) {
+        String lowerCaseUrl = url.toLowerCase();
+        if (!lowerCaseUrl.startsWith("https://") && !lowerCaseUrl.startsWith("http://")) {
+            return false;
+        }
+        try {
+            new URL(url);
+            return true;
+        } catch (MalformedURLException e) {
+            return false;
+        }
+    }
+}
--- support/samples/cds-cli-java/src/main/java/au/org/consumerdatastandards/cli/OffsetDateTimeConverter.java
+++ support/samples/cds-cli-java/src/main/java/au/org/consumerdatastandards/cli/OffsetDateTimeConverter.java
@@ -1,4 +1,4 @@
-package au.org.consumerdatastandards;
+package au.org.consumerdatastandards.cli;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
