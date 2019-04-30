18b453be1cf94e822c574960189b921a34f5c2ae
fyang1024 <fyang1024@gmail.com>
2019-04-30 14:03:42 +1000

    Payload Validator and CLI #17 #18 #19

---
+++ support/samples/cds-conformance/src/main/java/au/org/consumerdatastandards/conformance/Payload.java
@@ -0,0 +1,16 @@
+package au.org.consumerdatastandards.conformance;
+
+import au.org.consumerdatastandards.codegen.model.EndpointModel;
+import au.org.consumerdatastandards.support.EndpointResponse;
+
+public class Payload {
+
+    private PayloadType payloadType;
+
+    private Class<?> payloadClass;
+
+    private EndpointModel endpointModel;
+
+    private EndpointResponse endpointResponse;
+
+}
